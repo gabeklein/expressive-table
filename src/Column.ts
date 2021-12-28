0511b0f1fcb681d311803710d0966c4035a61682
Gabe Klein <gabe@termtm.com>
2021-12-28 11:40:24 -0500

    create fake-component column

---
+++ src/Column.ts
@@ -0,0 +1,16 @@
+interface Column {
+  context: any;
+  setState: any;
+  forceUpdate: any;
+  props: any;
+  state: any;
+  refs: any;
+}
+
+class Column {
+  render(){
+    return null;
+  }
+}
+
+export { Column }
