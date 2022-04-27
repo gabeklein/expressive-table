1398b3f38f62d6cba58fb39e64a74727cefd150c
Gabe Klein <gabe@termtm.com>
2022-04-27 13:02:15 -0400

    bundle types from source

--- rollup.config.js
+++ rollup.config.js
@@ -2,36 +2,49 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import dts from 'rollup-plugin-dts'
 
-export default  {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'lib/index.js',
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: 'lib/index.esm.js',
-      format: 'esm',
-      sourcemap: true
-    }
-  ],
-  external: [
-    '@expressive/mvc',
-    '@expressive/css',
-    'react'
-  ],
-  plugins: [
-    babel({
-      presets: ['@expressive/react'],
-      babelHelpers: "bundled"
-    }),
-    resolve({
-      extensions: ['.js', '.jsx', '.ts', '.tsx']
-    }),
-    commonjs(),
-    typescript()
-  ]
-}
+export default  [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'lib/index.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: 'lib/index.esm.js',
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    external: [
+      '@expressive/mvc',
+      '@expressive/css',
+      'react'
+    ],
+    plugins: [
+      babel({
+        presets: ['@expressive/react'],
+        babelHelpers: "bundled"
+      }),
+      resolve({
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }),
+      commonjs(),
+      typescript()
+    ]
+  },
+  {
+    input: './src/index.ts',
+    output: [{
+      file: 'lib/index.d.ts',
+      format: 'es'
+    }],
+    plugins: [
+      dts()
+    ]
+  },
+]
