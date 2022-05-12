b7aa49974af5cf2943e9afff89090b21f6da437e
Gabe Klein <gabe@termtm.com>
2022-05-11 23:48:23 -0400

    Split Body off from Table

--- src/Table.js
+++ src/Table.js
@@ -7,15 +7,7 @@ import Header from './Header';
 import Rows from './Rows';
 
 export const Table = (props) => {
-  const Empty = props.empty;
-  const {
-    style,
-    get: control,
-    virtual: {
-      container,
-      size
-    }
-  } = useModel(() => {
+  const control = useModel(() => {
     const source = props.for;
 
     if(!source)
@@ -27,37 +19,46 @@ export const Table = (props) => {
     if(Grid.isTypeof(source))
       return new source();
 
-    throw new Error("Table expects either instance or typeof Grid.")
-  });
-
-  control.import(props);
+    throw new Error("Table expects either an instance or typeof Grid.");
+  }, []);
 
   useLayoutEffect(() => {
     control.ready = true;
   }, []);
 
-  container: {
-    style = { ...props.style, ...style };
-    forward: className;
-    gridRows: min, "minmax(0, 1.0fr)";
-    overflow: hidden;
-  }
+  control.import(props);
 
   <Provider of={control}>
-    <container>
-      <Header {...props} />
-      <div
-        ref={size ? container : undefined}
-        style={{ overflowY: "auto" }}>
-        {props.before}
-        <Rows {...props} />
-        {props.after}
-        {props.children}
-      </div>
-    </container>
+    <Body {...props} />
   </Provider>
 }
 
 Object.assign(Table, components);
 
 export default Table;
+
+const Body = (props) => {
+  const {
+    style,
+    virtual: {
+      container,
+      size
+    }
+  } = Grid.tap();
+
+  forward: className;
+  gridRows: min, "minmax(0, 1.0fr)";
+  overflow: hidden;
+
+  <this style={{ ...props.style, ...style }}>
+    <Header {...props} />
+    <div
+      ref={size ? container : undefined}
+      style={{ overflowY: "auto" }}>
+      {props.before}
+      <Rows {...props} />
+      {props.after}
+      {props.children}
+    </div>
+  </this>
+}
