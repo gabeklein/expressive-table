2d4ef508d0d462489d8779b19035af584c0e3d3b
Gabe Klein <gabe@termtm.com>
2022-07-29 16:21:22 -0400

    make column size a constant after normalize

--- src/Column.ts
+++ src/Column.ts
@@ -34,11 +34,11 @@ declare namespace Column {
 }
 
 const Column = (props: Column.Props) => {
-  let { cell, head, name, render, size, value } = props;
+  let { cell, head, name, render, value } = props;
 
   const control = Grid.get();
   const column = useMemo(() => ({} as Grid.Column<Grid>), []);
-  size = normalSize(size);
+  const size = normalSize(props.size);
 
   if(value === undefined && name)
     value = name.toLowerCase();
