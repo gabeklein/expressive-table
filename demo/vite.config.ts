36d2d7f2b4d5cc624ba28c33a01c2afc5c175b58
Gabe Klein <gabe@termtm.com>
2025-02-14 12:39:24 -0500

    upgrade packages

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,10 +1,10 @@
 import jsx from '@expressive/vite-plugin-jsx';
 import react from '@vitejs/plugin-react';
-import Vite from 'vite';
+import { defineConfig } from 'vite';
 
-export default <Vite.UserConfig> {
+export default defineConfig({
   plugins: [
     react(),
     jsx()
   ]
-}
+})
