015ea999c0c895e47143cc97a511413f0838f6ed
Gabe Klein <gabe@termtm.com>
2025-03-04 15:18:57 -0500

    add build configuration for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,37 @@ import jsx from '@expressive/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  root: 'dev',
-  plugins: [
+export default defineConfig(({ command }) => {
+  const plugins = [
     jsx(),
     react({
       jsxImportSource: '@expressive/react',
       jsxRuntime: 'automatic'
     })
-  ]
-})
+  ];
+
+  if (command != 'build')
+    return {
+      plugins,
+      root: 'dev'
+    };
+
+  return {
+    plugins,
+    build: {
+      lib: {
+        entry: __dirname + '/src/index.ts',
+        formats: ['es'],
+        fileName: () => 'index.js'
+      },
+      rollupOptions: {
+        external: (id) => /^[^./]/.test(id),
+        output: {
+          assetFileNames: 'assets/[name][extname]'
+        }
+      },
+      cssCodeSplit: true,
+      outDir: 'dist'
+    }
+  };
+});
